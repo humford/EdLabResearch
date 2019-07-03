f2eab9d6549fdd84bf2fe086bf26eabf498b27b3
Henry Williams <htw2116@columbia.edu>
2019-07-03 09:54:56 -0400

    migrated files

--- ezproxy-research/network-modeling/doi-network-structure-map.py
+++
@@ -1,7 +0,0 @@
-from graph_tools.all import *
-
-doi_network = Graph()
-vertex_id = doi_network.new_vertex_property("string")
-doi_network.vp.id = vertex_id
-
-doi_network.new_vertex()
--- testing/refextract-test.py
+++
@@ -1,3 +0,0 @@
-from refextract import extract_references_from_url
-references = extract_references_from_url('https://arxiv.org/pdf/1503.07589.pdf')
-print(references[0])
